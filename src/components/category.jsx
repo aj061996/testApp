d0cb7f17199f0c7309f3e5d528a07c5904b401f6
ananth <j.ananthrdsm@gmail.com>
2023-11-26 20:55:40 +0530

    first commit

---
+++ src/components/category.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div>
+      these are the categories
+    </div>
+  )
+}
+
+export default Category
---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Category from '../category'
+
+const Homepage = () => {
+  return (
+    <div>
+        this is the home page
+        <Category/>
+    </div>
+  )
+}
+
+export default Homepage
